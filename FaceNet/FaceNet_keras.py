4b8e7888fc0393653bc3a39232d5ed7bb3b97d27
unknown <binglinli106@126.com>
2018-11-16 15:08:36 +0800

    added FaceNet

---
+++ FaceNet/FaceNet_keras.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+简介：
+实现人脸识别经典算法FaceNet
+
+Created on Fri Nov 16 14:59:31 2018
+
+@author: dell
+"""
+
+from keras.models import Sequential
+from keras.layers import Conv2D, ZeroPadding, Activation, Input, concatenate
+from keras.models import Model
+from keras
