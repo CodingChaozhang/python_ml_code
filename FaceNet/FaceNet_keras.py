57b80c14652eab30e2ded8ee1c102d96e2e25644
unknown <binglinli106@126.com>
2018-12-19 22:52:24 +0800

    added FaceNet

--- FaceNet/FaceNet_keras.py
+++ FaceNet/FaceNet_keras.py
@@ -16,6 +16,8 @@ from keras.models import Model
 from keras.initializers import glorot_uniform
 from keras.engine.topology import Layer
 from keras import backend as K
+import fr_utils
+from inception_blocks_v2 import *
 
 K.set_image_data_format('channels_first')
 
@@ -58,9 +60,72 @@ def triplet_loss(y_true, y_pred, alpha=0.2):
     return loss
 
 
+#创建模型
+FaceNet = faceRecoModel(input_shape=(3, 96, 96))
+
+#开始时间
+start_time = time.clock()
+
+#编译模型
+FaceNet.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
+
+#加载权值
+fr_utils.load_weights_from_FaceNet(FaceNet)
+
+#结束时间
+end_time = time.clock()
+
+#计算时差
+minium = end_time - start_time
+
+print("执行了：" + str(int(minium / 60)) + "分" + str(int(minium%60)) + "秒")
+
     
+database = {}
+database["danielle"] = fr_utils.img_to_encoding("images/danielle.png", FaceNet)
+database["younes"] = fr_utils.img_to_encoding("images/younes.jpg", FaceNet)
+database["tian"] = fr_utils.img_to_encoding("images/tian.jpg", FaceNet)
+database["andrew"] = fr_utils.img_to_encoding("images/andrew.jpg", FaceNet)
+database["kian"] = fr_utils.img_to_encoding("images/kian.jpg", FaceNet)
+database["dan"] = fr_utils.img_to_encoding("images/dan.jpg", FaceNet)
+database["sebastiano"] = fr_utils.img_to_encoding("images/sebastiano.jpg", FaceNet)
+database["bertrand"] = fr_utils.img_to_encoding("images/bertrand.jpg", FaceNet)
+database["kevin"] = fr_utils.img_to_encoding("images/kevin.jpg", FaceNet)
+database["felix"] = fr_utils.img_to_encoding("images/felix.jpg", FaceNet)
+database["benoit"] = fr_utils.img_to_encoding("images/benoit.jpg", FaceNet)
+database["arnaud"] = fr_utils.img_to_encoding("images/arnaud.jpg", FaceNet)
+
+def verify(image_path, identity, database, model):
+    """
+    对“identity”与“image_path”的编码进行验证。
+
+    参数：
+        image_path -- 摄像头的图片。
+        identity -- 字符类型，想要验证的人的名字。
+        database -- 字典类型，包含了成员的名字信息与对应的编码。
+        model -- 在Keras的模型的实例。
+
+    返回：
+        dist -- 摄像头的图片与数据库中的图片的编码的差距。
+        is_open_door -- boolean,是否该开门。
+    """
+    #第一步：计算图像的编码，使用fr_utils.img_to_encoding()来计算。
+    encoding = fr_utils.img_to_encoding(image_path, model)
+
+    #第二步：计算与数据库中保存的编码的差距
+    dist = np.linalg.norm(encoding - database[identity])
+
+    #第三步：判断是否打开门
+    if dist < 0.7:
+        print("欢迎 " + str(identity) + "回家！")
+        is_door_open = True
+    else:
+        print("经验证，您与" + str(identity) + "不符！")
+        is_door_open = False
+
+    return dist, is_door_open
+ 
     
-    
-    
+verify("images/camera_0.jpg","younes",database, FaceNet)    
     
     
